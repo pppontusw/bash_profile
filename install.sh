2b9eeedf832bd58205042ea55f1b3b5de237449f
Pontus Wiberg <pontus@pontusw.com>
2019-09-07 18:50:45 +0300

    Install vimrc and tmux conf

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cp profile ~/.bash_profile
 source ~/.bash_profile
+cp .vimrc ~/.vimrc
+cp .tmux.conf ~/.tmux.conf 
--- linux-install.sh
+++ linux-install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cp profile ~/.bash_aliases
 source ~/.bash_aliases
+cp .vimrc ~/.vimrc
+cp .tmux.conf ~/.tmux.conf 
